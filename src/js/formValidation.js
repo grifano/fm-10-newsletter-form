a4e35b0c90636317254030ccc3d1449c158281c8
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-10-09 07:11:41 +0200

    add modal message

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -1,4 +1,6 @@
 const input = document.querySelector('input');
+const modalMsg = document.querySelector('.modal-msg');
+
 input.addEventListener('click', e => {
   e.currentTarget.classList.toggle('isInvalid');
 });
